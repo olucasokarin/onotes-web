3201111bb344ade3a37661f0e177e8df38352b44
olucasokarin <olucas014@hotmail.com>
2020-06-01 16:46:15 -0300

    started singin screen

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Content } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>LOGON</h1>
+        <p>Yours notes in anywhere</p>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Content = styled.div`
+  width: 100%;
+  max-width: 720px;
+  height: 680px;
+  background: #312e38;
+  border-radius: 10px;
+`;
